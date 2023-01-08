3eb4eb5144517ff82cfbf260bf2dc69b8ab9ee5f
Hattrick HttrckCldHKS <hattrick.claudiuhks@gmail.com>
2023-01-08 11:37:03 +0200

    remove _STAT_VER check

--- hgsm_csgo_sse2/hack_fstat.c
+++ hgsm_csgo_sse2/hack_fstat.c
@@ -6,12 +6,6 @@
 
 #include <unistd.h>
 
-#ifndef _STAT_VER
-
-#define _STAT_VER 3 /** _STAT_VER_KERNEL64 / _STAT_VER_GLIBC2_3_4 / _STAT_VER_LINUX */
-
-#endif
-
 __asm__(".symver fstat, __fxstat@GLIBC_2.0");
 
 int __wrap_fstat(int _0, struct stat* _1)
